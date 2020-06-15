46b3fb6c06e604d813315bdbee889730be7c1bf3
kosta-design <66972581+kosta-design@users.noreply.github.com>
2020-06-15 23:27:08 +0200

    Add files via upload
    Updates are coming. I might re-make the whole loadingscreen
    for a more simple understanding.
    
    Features
    
    Fullscreen YouTube video
    Background music
    Animated Plane
    Animated Ghost
    Animated Buttons

---
+++ __resource.lua
@@ -0,0 +1,32 @@
+files {
+    'loading.html',
+    'css/bootstrap.min.css',
+    'css/css.css',
+    'css/demo.css',
+    'css/font.css',
+    'css/fontawesome-all.min.css',
+    'css/reset.css',
+    'css/style.css',
+    'css/style_common.css',
+    'css/style-dark.css',
+    'css/style1.css',
+    'img/back001.jpg',
+    'img/cursor.png',
+    'img/discord-icon.png',
+    'img/discord-icon2.png',
+    'img/discord-logo.png',
+    'img/gta.jpg',
+    'img/png.png',
+    'img/white_texture.jpg',
+    'js/cursor.js',
+    'js/main.js',
+    'js/music-controls.js',
+    'js/music-handler.js',
+    'js/video.js',
+    'js/script.js',
+    'audi.mp3',
+}
+
+loadscreen 'loading.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
