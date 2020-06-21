e4180f0c0bd5dac9d6964a2fb345e2104ccaa722
kosta-design <66972581+kosta-design@users.noreply.github.com>
2020-06-21 09:25:57 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,33 @@
+files {
+    'loading.html',
+    'css/bootstrap.min.css',
+    'css/css.css',
+    'css/demo.css',
+    'css/font.css',
+    'css/fontawesome-all.min.css',
+    'css/reset.css',
+    'css/style.css',
+    'css/style_common.css',
+    'css/style-dark.css',
+    'css/style1.css',
+    'img/back001.jpg',
+    'img/cursor.png',
+    'img/discord-icon.png',
+    'img/discord-icon2.png',
+    'img/discord-logo.png',
+    'img/gta.jpg',
+    'img/png.png',
+    'img/white_texture.jpg',
+    'js/cursor.js',
+    'js/main.js',
+    'js/video.js',
+    'js/script.js',
+    'music-controls.js',
+    'music-handler.js',
+    'audi.mp3',
+    'vid/LoadingScreen.mp4'
+}
+
+loadscreen 'loading.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
